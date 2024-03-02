d8e61e05b9b7d5734fe2f046fa2f793e02addee1
Kaitlynn Clement <kmc1191@rit.edu>
2024-03-02 17:23:03 -0500

    Add show dialog always true

--- wichacks-2024/src/App.js
+++ wichacks-2024/src/App.js
@@ -13,6 +13,7 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const SCOPES = "user-top-read"; 
+  const SHOW_DIALOG = true;
 
 
   const [token, setToken] = useState("")
@@ -60,7 +61,7 @@ function App() {
         <h1>Spotify React App</h1>
         { !token ?
 
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`}>Login to Spotify</a>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}&show_dialog=${SHOW_DIALOG}`}>Login to Spotify</a>
         : <button onClick = {logout} >Log Out</button>} 
         </header>
     </div>
