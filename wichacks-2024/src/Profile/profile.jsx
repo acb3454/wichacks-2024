25e0ac8db95fbf754d061947e0548b77d4ed8508
Aemilia <acb3454@rit.edu>
2024-03-02 16:06:09 -0500

    some profile stuff

--- wichacks-2024/src/Profile/profile.jsx
+++ wichacks-2024/src/Profile/profile.jsx
@@ -1,8 +1,12 @@
 // Profile.js
 import React, { useEffect, useState } from "react";
 
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const ARTISTS_URL = "https://api.spotify.com/v1/me/top/artists?time_range=long-term&limit=10&offset=5"
+
 const Profile = ({ token }) => {
   const [userInfo, setUserInfo] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
 
   useEffect(() => {
     // Fetch user information using the Spotify API
@@ -25,8 +29,28 @@ const Profile = ({ token }) => {
       }
     };
 
+    const fetchTopArtists = async () => {
+        try {
+          const response = await fetch("https://api.spotify.com/v1/me/top/artists?offset=0&limit=10&time_range=long_term", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          if (response.ok) {
+            const data = await response.json();
+            setTopArtists(data);
+          } else {
+            console.error("Failed to fetch top artists");
+          }
+        } catch (error) {
+          console.error("Error fetching top artists:", error);
+        }
+      };
+
     if (token) {
       fetchUserInfo();
+      fetchTopArtists();
     }
   }, [token]);
 
@@ -35,6 +59,7 @@ const Profile = ({ token }) => {
       {userInfo ? (
         <div>
           <h2>Welcome, {userInfo.display_name}!</h2>
+          <h3>Your top Artists: {topArtists}</h3>
           {/* Display other user information as needed */}
         </div>
       ) : (
