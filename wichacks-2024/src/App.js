accb0c70d426c4359dcc69783505c43878feae24
Aemilia <acb3454@rit.edu>
2024-03-02 14:54:08 -0500

    login and logout are working!

--- wichacks-2024/src/App.js
+++ wichacks-2024/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
@@ -7,12 +8,38 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
+  const [token, setToken] = useState("")
+
+  useEffect( ()=>{
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(!token && hash){
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      console.log(token)
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      
+    }
+
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify React App</h1>
+        { !token ?
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-      </header>
+        : <button onClick = {logout} >Log Out</button>}
+        </header>
     </div>
   );
 }
