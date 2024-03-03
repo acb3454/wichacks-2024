4c50b9337f58f9e7979aeb9bdd70f5e9e22e58af
Peyton Wagner <paw7341@g.rit.edu>
2024-03-02 22:45:42 -0500

    home stuff

--- wichacks-2024/src/Home/AboutUs/Home.js
+++ wichacks-2024/src/Home/AboutUs/Home.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Home() {
   const CLIENT_ID = "470974f837054eeea1df14cebc95f27f"
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
+
 
   return (
     <div className="App">
