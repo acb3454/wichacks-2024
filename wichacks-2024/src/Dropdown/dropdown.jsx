4bcf9f55581a26d0e6c9f6a9fdfb6da08b550d63
Aemilia <acb3454@rit.edu>
2024-03-03 03:01:10 -0500

    tags are getting added to json!

--- wichacks-2024/src/Dropdown/dropdown.jsx
+++ wichacks-2024/src/Dropdown/dropdown.jsx
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-export default function BasicExample() {
+export default function BasicExample({ onTagSelect }) {
+  const [selectedTag, setSelectedTag] = useState(null);
+
+  const handleTagSelect = (tag) => {
+    setSelectedTag(tag);
+    // Notify the parent component about the selected tag
+    onTagSelect(tag);
+  };
+
   return (
-    <Dropdown>
+    <Dropdown onSelect={handleTagSelect}>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
-        that feeling when...
+        {selectedTag || "Select Tag"}
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">you're falling in love</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">you're heartbroken</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">you're being ghosted</Dropdown.Item>
+        <Dropdown.Item eventKey="love">you're falling in love</Dropdown.Item>
+        <Dropdown.Item eventKey="heartbroken">you're heartbroken</Dropdown.Item>
+        <Dropdown.Item eventKey="ghosted">you're being ghosted</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
