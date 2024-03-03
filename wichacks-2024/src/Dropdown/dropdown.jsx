3bf943baba851528b64daa174f35740aebe7a082
Peyton Wagner <paw7341@g.rit.edu>
2024-03-03 05:18:12 -0500

    SUPER SLAY YAY

--- wichacks-2024/src/Dropdown/dropdown.jsx
+++ wichacks-2024/src/Dropdown/dropdown.jsx
@@ -20,12 +20,16 @@ export default function BasicExample({ onTagSelect }) {
         <Dropdown.Item eventKey="that feeling when you're falling in love">you're falling in love</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you're feeling heartbroken">you're heartbroken</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you're being ghosted">you're being ghosted</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you're relaxing">you're relaxing</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you're no contact">you're no contact</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you face imposter syndrome">you face imposter syndrome</Dropdown.Item>
-        <Dropdown.Item eventKey="that feeling when you're happy">you're happy</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you're slaying">you're slaying</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you're a gym girly">you're a gym girly</Dropdown.Item>
         <Dropdown.Item eventKey="that feeling when you need to study">you need to study</Dropdown.Item>
-        <Dropdown.Item eventKey="that feeling when you need a hype man">you need a hype man</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you're on an adventure">you're on an adventure</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you need a cheerleader">you need a cheerleader</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you fumble the bag">you fumble the bag</Dropdown.Item>
+        <Dropdown.Item eventKey="that feeling when you need a dance party">you need a dance party</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
